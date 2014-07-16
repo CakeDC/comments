2d92a694ef8f777d56e2a1e686050bf22a27e68b
Florian Krämer <florian.kraemer@cakedc.com>
2014-07-17 01:08:27 +0200

    Updating the copyright year

--- Model/Behavior/CommentableBehavior.php
+++ Model/Behavior/CommentableBehavior.php
@@ -2,14 +2,14 @@
 /**
  * CakePHP Comments
  *
- * Copyright 2009 - 2013, Cake Development Corporation
+ * Copyright 2009 - 2014, Cake Development Corporation
  *                        1785 E. Sahara Avenue, Suite 490-423
  *                        Las Vegas, Nevada 89104
  *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright 2009 - 2013, Cake Development Corporation
+ * @Copyright 2009 - 2014, Cake Development Corporation
  * @link      http://codaset.com/cakedc/migrations/
  * @package   plugins.comments
  * @license   MIT License (http://www.opensource.org/licenses/mit-license.php)
